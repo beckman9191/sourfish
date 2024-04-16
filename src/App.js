a0e1311de646d3eab79958178e4e4bec584111fc
beckman9191 <z263zhu@uwaterloo.ca>
2024-04-16 12:13:04 -0400

    finished-home-page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 function App() {
   return (
-    <div className="app">
-      
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+
+    <Router>
+      <div className="app">
+        {/* Header */}
+        <Header />
+        <Routes>
+
+          <Route path="/" 
+          element={
+            <>
+            <Home />
+            
+            </>
+          } />
+
+
+          <Route path="/checkout" 
+          element={
+            <h1>I AM A CHECKOUT</h1>
+          } />
+        </Routes>
+        
+        </div>
+    </Router>
+    
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -16,19 +16,62 @@ function Home() {
 
       <div className="home__row">
         {/* product */}
-        <Product />
+        <Product 
+        id="12321341"
+        title='The lean startup' 
+        price={29.99}
+        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        rating={5}
+        />
         {/* product */}
-        <Product />
+        <Product 
+        id="49538094"
+        title="Kenwood kMix Stand Mixer for 
+        Baking, Stylish Kitchen Mixer with 
+        K-beats, Dough Hook and Whisk, 5 Litre
+        Glass Bowl"
+        price={239.0}
+        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        rating={4}
+        />
       </div>
 
       <div className="home__row">
         {/* product */}
+        <Product 
+        id="4903850"
+        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+        price={199.99}
+        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        rating={3}
+        />
         {/* product */}
+        <Product 
+        id="23445930"
+        title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+        price={98.99}
+        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        rating={5}
+        />
         {/* product */}
+        <Product 
+        id="3254354345"
+        title="New Apple Ipad pro"
+        price={598.99}
+        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        rating={4}
+        />
       </div>
 
       <div className="home__row">
         {/* product */}
+        <Product 
+        id="90829332"
+        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+        price={1094.98}
+        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        rating={4}
+        />
       </div>
     </div>
   )
